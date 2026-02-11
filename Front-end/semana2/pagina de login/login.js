5ba4395a58ee6acff95ffd7634c7d8b0380d3c4e
Luiz Gustavo <xkisol318@gmail.com>
2026-02-11 19:42:02 -0300

    update inputs

--- Front-end/semana2/pagina de login/login.js
+++ Front-end/semana2/pagina de login/login.js
@@ -6,11 +6,23 @@ function validarPassword(password) {
 }
 function showpassword() {
   const inputpassword = document.getElementById("input-senha");
-
+  // document.getElementById(senha).type = tipo_atual === "text" ? "password" : "text"
   if (inputpassword.type === "password") {
     inputpassword.type = "text";
   } else {
     inputpassword.type = "password";
+  }
+}
+function perceberErro() {
+  let inputPassword = document.getElementById("input-senha");
+  let inputEmail = document.getElementById("input-email");
+  // usando 3 sinais de === para comparar tanto o valor quanto o tipo da informacao
+  if (inputPassword.value === "") {
+    inputPassword.style.backgroundColor = "#cf101042";
+  }
+  if (inputEmail.value === "") {
+    inputEmail.style.backgroundColor = "#cf101042";
+
   }
 }
 function validarAll() {
